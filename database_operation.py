fe66aba2a23ecd263ac68473bca9e08a556a84f8
Karush2807 <arushkarnatak1881@gmail.com>
2024-05-27 18:01:49 +0530

    addded comments to code

--- database_operation.py
+++ database_operation.py
@@ -3,38 +3,43 @@ from bson import ObjectId
 from bson.binary import Binary
 from encryption import encrypt_password, decrypt_password
 
+# Connect to the MongoDB database using the provided connection string
 client=MongoClient("mongodb+srv://nigga2807:244466666@youtubemanager.8ssqukg.mongodb.net/") #this will link python to our mongodb database
+
+# Access the specific database within MongoDB
 data_base=client["PasswordManager"] #ye jo hmara database hai, uska name hai
+
+# Access the specific collection within the database
 pass_collections=data_base['PassStorage'] #ye jo hmne uske andar colllection bnayi hai, vo hai ye
 
 #function to add password
 def add_password(platform, username, password):
-    encrypted_password=encrypt_password(password)
+    encrypted_password=encrypt_password(password) #encrypting the password
     pass_collections.insert_one({'Platform Name': platform,
                                 "UserName": username,
-                                "Password": Binary(encrypted_password)})
+                                "Password": Binary(encrypted_password)}) #storing the encrypted password as binary
     print("data added succesfully :)")
 
 #function to update the existinng password!!
 def update_password(id, password):
-    encrypted_password=encrypt_password(password)
+    encrypted_password=encrypt_password(password) #encrypt the new password
     pass_collections.update_one(
-        {'_id': ObjectId(id)},
-        {"$set": {'Password': encrypted_password}}
+        {'_id': ObjectId(id)}, #find the password with the given id
+        {"$set": {'Password': encrypted_password}} #update the password with the new encrypted password
     )
     print("Passwords updated succesfully :)")
 
 #function to delete password!!
 def delete_password(id):
     pass_collections.delete_one(
-        {'_id': ObjectId(id)},
+        {'_id': ObjectId(id)}, #find the password with the given id
     )
     print("Data deleted succesfully :)")
 
 #function to show list of all passwords
 def show_passwords():
-    for wordpass in pass_collections.find():
-        decrypted_password = decrypt_password(wordpass['Password'])
+    for wordpass in pass_collections.find(): #iterate over entry in the collection
+        decrypted_password = decrypt_password(wordpass['Password']) #decrypt the password
 
         print(
             f"ID: {wordpass['_id']},"
--- encryption.py
+++ encryption.py
@@ -1,23 +1,26 @@
 from cryptography.fernet import Fernet
-#will take a input as string and complete it into a complete random character!!
 
-#one-time run hoga, bs key generate krne ke liye
+# Function to write the key (run this once to generate the key)
 # def write_key():
-#     key= Fernet.generate_key()#thisk key must be safe, as anyone with  key can decrypt data
+#     key = Fernet.generate_key()  # Generate a new encryption key
 #     with open("key.key", "wb") as key_file:
-#         key_file.write(key)
-# write_key() #this will generate a key file in the same directory as this file
+#         key_file.write(key)  # Save the key to a file
 
+# Load the encryption key
 def load_key():
-    return open("key.key", "rb").read()
+    return open("key.key", "rb").read()  # Read the key from the file
 
+# Load the key from the file
 key = load_key()
-cipher_suite = Fernet(key)
+cipher_suite = Fernet(key)  # Initialize Fernet with the loaded key
 
+# Function to encrypt a password
 def encrypt_password(password):
-    encrypted_pass=cipher_suite.encrypt(password.encode())
+    encrypted_pass = cipher_suite.encrypt(password.encode())  # Encrypt the password
     return encrypted_pass
 
+# Function to decrypt an encrypted password
 def decrypt_password(encrypt_pass):
-    decrypted_password = cipher_suite.decrypt(encrypt_pass.decode())
-    return decrypted_password.decode()
+    decrypted_password = cipher_suite.decrypt(encrypt_pass)  # Decrypt the password
+    return decrypted_password.decode()  # Decode the decrypted password to string
+
